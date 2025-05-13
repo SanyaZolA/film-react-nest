5d30dacd671103c59c5c623fb2bba79988386d9c
Alexandr <ntl1221@yandex.ru>
2025-05-13 15:27:29 +0300

    v.2.2.1 add Docker

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   port: process.env.PORT || 3000,
   db: {
-    host: process.env.DATABASE_HOST || 'localhost',
+    host: process.env.DATABASE_HOST || 'postgres',
     port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-    user: process.env.DATABASE_USER || 'postgres',
+    user: process.env.DATABASE_USER || 'admin',
     password: process.env.DATABASE_PASSWORD || 'admin',
     name: process.env.DATABASE_NAME || 'films',
     schema: process.env.DATABASE_SCHEMA || 'public',
