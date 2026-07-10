6768652f6e396bf130bc8ba349cf2fe03464a8e6
Alexandr <ntl1221@yandex.ru>
2026-07-10 15:01:52 +0300

    fix styles. fix vulnerabilities

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        includePaths: ['src/scss']
-      }
-    }
-  },
   server: {
-    host: 'localhost',
-    port: 5173
-  }
-})
+    host: "localhost",
+    port: 5173,
+  },
+});
